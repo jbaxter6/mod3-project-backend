b1fa00441c5898e72bacd1c496a63a76809dcf53
Catrina Friday <cat.friday@gmail.com>
2020-07-28 14:58:20 -0400

    gitignore added

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,15 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  has_many :user_interests
-  has_many :interests, through: :user_interests
-
-  has_many :user_preferences
-  has_many :preferences, through: :user_preferences
-  
-  has_many :sent_matches, foreign_key: :matcher_id, class_name: 'Match'
-  has_many :matchers, through: :sent_matches
-
-  has_many :received_matches, foreign_key: :matchee_id, class_name: 'Match'
-  has_many :matchees, through: :received_matches
 
   attributes :id, :name, :age, :gender, :smoker, :has_kids, :tag_line, :image, :interests, :preferences
 
